a109dbee4d26bbcee2d39fbef3dbf3b93933351f
jaeuk520 <jaeuk520@naver.com>
2023-12-19 18:45:22 +0900

    feat: reservation 도메인 생성

---
+++ src/main/java/com/example/catchtable/model/Reservation.java
@@ -0,0 +1,33 @@
+package com.example.catchtable.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity(name = "reservation")
+@Getter
+@NoArgsConstructor
+public class Reservation {
+
+    @Id
+    @Column(name = "reservation_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Long reservationId;
+
+    @Column(name = "date")
+    private Timestamp date;
+
+    @Column(name = "time")
+    private Timestamp time;
+
+    @Column(name = "count_people")
+    private Long countPeople;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+}
